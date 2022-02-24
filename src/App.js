b700943c7ad95c8930a7ffde51dc29408d28be4f
Almog Lev <almoglev3@gmail.com>
2022-02-24 22:50:56 +0200

    Navbar component + fetching data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 // styles
 import './App.css'
 
 // pages
-import Create from './pages/create/Create';
-import Home from './pages/home/Home';
-import Recipe from './pages/recipe/Recipe';
-import Search from './pages/search/Search';
+import Navbar from './components/Navbar'
+import Create from './pages/create/Create'
+import Home from './pages/home/Home'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar></Navbar>
         <Switch>
           <Route exact path="/">
             <Home />
