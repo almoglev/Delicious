8836963a4737c95a25f54489e6527028c991e56e
Almog Lev <almoglev3@gmail.com>
2022-02-25 19:09:45 +0200

    Search results page

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,37 @@
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
 // styles
 import './Search.css'
 
+// components
+import RecipeList from '../../components/RecipeList'
+import Spinner from '../../components/Spinner'
+
 function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q').trim()
+
+  const url = 'http://localhost:3000/recipes?q=' + query
+  const {error, isPending, data} = useFetch(url)
+
+  if (query === ""){
+    return  <h2 className="page-title">Sorry, no results</h2>
+  }
+
   return (
-    <div>Search</div>
+    <div>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'><Spinner /></p>}
+      {data && data.length > 0 ? 
+        <>
+          <h2 className="page-title">Recipes including "{query}"</h2>
+          <RecipeList recipes={data} />
+        </>
+      : 
+        <h2 className="page-title">Sorry, no results</h2>}
+    </div>
   )
 }
 
