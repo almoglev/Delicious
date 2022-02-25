9c4b207e4bdaa73410f631dd890a96b7802ee3d9
Almog Lev <almoglev3@gmail.com>
2022-02-25 22:26:00 +0200

    Changing colors with Reducers and useReducer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ import { useTheme } from '../hooks/useTheme'
 import './Navbar.css'
 
 function Navbar() {
-  const { color } = useTheme()
-  
+  const { color, changeColor } = useTheme()
+
   return (
     <div className='navbar' style= {{ background: color}}>
-        <nav>
+        <nav onClick={() => changeColor('pink')}>
             <Link to='/' className='brand'>
                 <h1>
                   <img src={burger} alt="" width="40px"/>&nbsp;
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,10 +1,32 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 
 export const ThemeContext = createContext()
 
+// first argumet- current up to date state, second argument- action object from the dispatch call
+const themeReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            // spread existing state and override the color property
+            return { ...state, color: action.payload}
+        default:
+            return state
+    }
+}
+
 export function ThemeProvider({ children }) {
-    return (
-        <ThemeContext.Provider value={{ color: 'blue' }}>
+    // first argument- reducer func, second argument- initial state. 
+    // useReducer returns a state, and dispatch. the dispatch call the function inside useReducer, themeReducer in this case
+    const [state, dispatch] = useReducer(themeReducer, {
+        color: 'blue'
+    })
+
+    const changeColor = (color) => {
+        // the dispatch will fire themeReducer, it takes as argument an action object that contains type and payload
+        dispatch({ type: 'CHANGE_COLOR', payload: color})
+    }
+
+    return (        
+        <ThemeContext.Provider value={{ ...state, changeColor }}>
             {children}
         </ThemeContext.Provider>
     )
